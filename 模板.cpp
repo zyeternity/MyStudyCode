4913e7db03a28e7b0d7e2db545dd19a60819dbd3
lgloryl <a15131807278@163.com>
2022-10-14 17:45:23 +0800

    modified:   "\346\250\241\346\235\277.cpp"

--- 模板.cpp
+++ 模板.cpp
@@ -41,6 +41,15 @@ inline void readlld(long long &k) {
     return;
 }
 
+inline void write(int x) {
+  static int sta[35];
+  int top = 0;
+  do {
+    sta[top++] = x % 10, x /= 10;
+  } while (x);
+  while (top) putchar(sta[--top] + 48);
+}
+
 int main() {
     std::ios::sync_with_stdio(false);
     // freopen("main.in", "r", stdin);
