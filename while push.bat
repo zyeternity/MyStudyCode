06c21cb980b8283e41fe32fa90a708f2f03fd47f
lgloryl <a15131807278@163.com>
2022-10-14 16:31:39 +0800

    new

--- while push.bat
+++ while push.bat
@@ -1,5 +1,10 @@
 @echo off
+set http_proxy=http://127.0.0.1:7890 & set https_proxy=http://127.0.0.1:7890
+git config --global user.email "a15131807278@163.com"
+git config --global user.name "lgloryl"
 :push
-push.bat
+git add ./
+git commit -am "new"
+git push origin main
 timeout -t 10
 goto push
