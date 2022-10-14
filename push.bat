19be7691613daf58841948471dd4d884283acccf
lgloryl <a15131807278@163.com>
2022-10-14 16:30:23 +0800

    new

--- push.bat
+++ push.bat
@@ -1,6 +1,8 @@
+@echo off
 git config --global user.email "a15131807278@163.com"
 git config --global user.name "lgloryl"
 git add ./
 git commit -am "new"
 set http_proxy=http://127.0.0.1:7890 & set https_proxy=http://127.0.0.1:7890
 git push origin main
+exit
