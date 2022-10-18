9fe3897e4461533a9ec842242ac92ef33ff97a53
lgloryl <a15131807278@163.com>
2022-10-18 16:53:40 +0800

    new

---
+++ 联考/10.08最后一次联考/BXX30/T4/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace IO_ios {
+#define MAXN 100
+struct ios_in {
+    template <typename _Tp> inline ios_in &operator>>(_Tp &x) {
+        char ch = getchar(), sgn = 0;
+        x = 0;
+        while (ch ^ '-' && !isdigit(ch))
+            ch = getchar();
+        if (ch == '-')
+            ch = getchar(), sgn = 1;
+        while (isdigit(ch))
+            x = x * 10 + ch - '0', ch = getchar();
+        if (sgn)
+            x = -x;
+        return *this;
+    }
+} Cin;
+
+struct ios_out {
+    template <typename _Tp> inline void operator<<(_Tp &x) {
+        char F[MAXN];
+        _Tp tmp = x > 0 ? x : (putchar('-'), -x);
+        int cnt = 0;
+        while (tmp) {
+            F[cnt++] = tmp % 10 + '0';
+            tmp /= 10;
+        }
+        while (cnt)
+            putchar(F[--cnt]);
+    }
+} Cout;
+} // namespace IO_ios
+
+using namespace IO_ios;
+
+int main() {
+    // std::ios::sync_with_stdio(false);
+    // freopen("main.in", "r", stdin);
+    // freopen("main.out", "w", stdout);
+
+    // int n;
+    // Cin >> n;
+    // Cout << n;
+
+    // fclose(stdin);
+    // fclose(stdout);
+    return 0;
+}
