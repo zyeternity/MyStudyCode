f37b79136cb018108af87a5467bf6c372a4d3923
lgloryl <a15131807278@163.com>
2022-10-19 20:53:47 +0800

    new

--- 牛客/2022牛客OI赛前集训营-普及组（第四场）/安平-049/B/B.cpp
+++ 牛客/2022牛客OI赛前集训营-普及组（第四场）/安平-049/B/B.cpp
@@ -1,56 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-namespace IO_ios {
-#define MAXN 100
-struct ios_in {
-    template <typename _Tp> inline ios_in &operator>>(_Tp &x) {
-        char ch = getchar(), sgn = 0;
-        x = 0;
-        while (ch ^ '-' && !isdigit(ch))
-            ch = getchar();
-        if (ch == '-')
-            ch = getchar(), sgn = 1;
-        while (isdigit(ch))
-            x = x * 10 + ch - '0', ch = getchar();
-        if (sgn)
-            x = -x;
-        return *this;
-    }
-} Cin;
-
-struct ios_out {
-    template <typename _Tp> inline void operator<<(_Tp &x) {
-        char F[MAXN];
-        _Tp tmp = x > 0 ? x : (putchar('-'), -x);
-        int cnt = 0;
-        while (tmp) {
-            F[cnt++] = tmp % 10 + '0';
-            tmp /= 10;
-        }
-        while (cnt)
-            putchar(F[--cnt]);
-    }
-} Cout;
-} // namespace IO_ios
-
-using namespace IO_ios;
-
 #define max_n (100005+10)
 
-long long n,x,y;
+long long n;
 long long ans = 1e18;
 long long jin[max_n],fang[max_n];
 
 int main() {
     
-    // freopen("B.in","r",stdin);
-    // freopen("B.out","w",stdout);
+    freopen("B.in","r",stdin);
+    freopen("B.out","w",stdout);
 
-    Cin >> n;
+    jin[0] = fang[0] = 0;
+
+    scanf("%lld",&n);
     for (int i = 1; i <= n; i++)
     {
-        char c = getchar();
+        char c;
+        // scanf("%c",&c);
+        cin>>c;
         jin[i] = jin[i- 1];
         fang[i] = fang[i- 1];
         if (c == '1')fang[i] += i;
@@ -59,14 +28,13 @@ int main() {
     
     for (int i = 0; i <= n; i++)
     {
-        ans = min(ans,(long long)(jin[i],fang[n] - fang[i]));
+        ans = min(ans,abs((long long)(jin[i] - fang[n] + fang[i])));
     }
 
-    Cout << ans;
+    printf("%lld\n", ans);
 
-    printf("\n");
-    // fclose(stdin);
-    // fclose(stdout);
+    fclose(stdin);
+    fclose(stdout);
 
     return 0;
 }
